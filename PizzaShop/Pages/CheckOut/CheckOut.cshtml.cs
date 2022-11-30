89dd605aa6b4c47e93fdc5665f84dca024695d34
datdtnhcse <dat.dtnhcse@gmail.com>
2022-11-30 23:21:47 +0700

    display data from URL

--- PizzaShop/Pages/CheckOut/CheckOut.cshtml.cs
+++ PizzaShop/Pages/CheckOut/CheckOut.cshtml.cs
@@ -3,10 +3,22 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace PizzaShop.Pages.CheckOut
 {
+    [BindProperties(SupportsGet = true)]
     public class CheckOutModel : PageModel
     {
+        public string PizzaName { get; set; }
+        public float PizzaPrice { get; set; }
+        public string ImageTitle { get; set; }
         public void OnGet()
         {
+            if (string.IsNullOrWhiteSpace(PizzaName))
+            {
+                PizzaName = "Custom";
+            }
+            if (string.IsNullOrWhiteSpace(ImageTitle))
+            {
+                ImageTitle = "Create";
+            }
         }
     }
 }
--- PizzaShop/Pages/Forms/CustomPage.cshtml.cs
+++ PizzaShop/Pages/Forms/CustomPage.cshtml.cs
@@ -12,6 +12,20 @@ namespace PizzaShop.Pages.Forms
         public void OnGet()
         {
         }
-        
+        public IActionResult OnPost()
+        {
+            PizzaPrice = Pizza.BasePrice;
+
+            if (Pizza.TomatoSauce) PizzaPrice++;
+            if (Pizza.Cheese) PizzaPrice++;
+            if (Pizza.Peperoni) PizzaPrice++;
+            if (Pizza.Mushroom) PizzaPrice++;
+            if (Pizza.Tuna) PizzaPrice++;
+            if (Pizza.Pineapple) PizzaPrice += 10;
+            if (Pizza.Ham) PizzaPrice++;
+            if (Pizza.Beef) PizzaPrice++;
+
+            return RedirectToPage("/CheckOut/CheckOut", new {Pizza.PizzaName, PizzaPrice});
+        }
     }
 }
