0ac040347e85611400177821f7e0da2acdc9e582
datdtnhcse <dat.dtnhcse@gmail.com>
2022-11-30 16:32:12 +0700

    first commit

---
+++ PizzaShop/Pages/CheckOut/CheckOut.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PizzaShop.Pages.CheckOut
+{
+    public class CheckOutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ PizzaShop/Pages/Forms/ThankYou.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PizzaShop.Pages.Forms
+{
+    public class ThankYouModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
